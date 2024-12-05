9ffc1f3d572b28f8b0f5099bc0ab199bad23647a
fusion <fusion@MacBook-Pro-Fusion-4.local>
2024-12-05 14:15:05 +0300

    init commit

---
+++ src/main/java/com/fusiontech/entity/AddressData.java
@@ -0,0 +1,36 @@
+package com.fusiontech.entity;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(indexName = "address")
+public class AddressData {
+  @Id
+  private UUID id;
+  private String address;
+  private String postcode;
+  private String latitude;
+  private String longitude;
+  private String buildingNumber;
+
+  @Override
+  public String toString() {
+	return "AddressData{" +
+		"id=" + id +
+		", address='" + address + '\'' +
+		", postcode='" + postcode + '\'' +
+		", latitude='" + latitude + '\'' +
+		", longitude='" + longitude + '\'' +
+		", buildingNumber='" + buildingNumber + '\'' +
+		'}';
+  }
+}
