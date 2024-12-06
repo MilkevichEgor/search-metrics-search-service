2f021d1dc0e2c024056d271595e6e9f72f39c3ec
fusion <fusion@MacBook-Pro-Fusion-4.local>
2024-12-06 14:16:48 +0300

    added elastic controller

--- src/main/java/com/fusiontech/service/AddressDataService.java
+++ src/main/java/com/fusiontech/service/AddressDataService.java
@@ -1,11 +1,7 @@
 package com.fusiontech.service;
 
-import com.fusiontech.entity.AddressData;
-import com.fusiontech.repository.AddressDataElasticsearchRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +9,4 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class AddressDataService {
 
-  private final AddressDataElasticsearchRepository addressDataElasticsearchRepository;
-
-  public Page<AddressData> search(String query, Pageable pageable) {
-	return addressDataElasticsearchRepository.getAddressDataByAddress(query, pageable);
-  }
 }
